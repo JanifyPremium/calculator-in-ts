90aee06c94869a18a56c7eb4151cb10a098207bc
Janify <107705505+JanifyPremium@users.noreply.github.com>
2024-04-03 12:04:02 +0200

    adding files
    Main.html, ts file, stylesheet added
    wip

---
+++ app.ts
@@ -0,0 +1,22 @@
+//document.getElementById('result').addEventListener('click', display);
+//import
+const button = document.getElementById('result');
+function diplay(val){
+
+    let a = document.getElementById('result') += val 
+    return val;
+    console.log(val);
+};
+
+function solve() {
+    let x = document.getElementById('result') ;
+    let y = eval(x);
+
+    document.getElementById('result') = y;
+        return y;
+};
+
+function clearScreen(){
+    document.getElementById('result').wid= '';
+};
+
